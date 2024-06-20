68375027d6976392984dc0d8ac9707bd309bb23f
elyasamri <elyas.amri@gmail.com>
2024-06-20 13:08:28 +0300

    add layout and boilerplate

--- layouts/UserLayout.tsx
+++ layouts/UserLayout.tsx
@@ -1,15 +1,12 @@
-import UserLayout from "@/layouts/UserLayout";
+import {PropsWithChildren} from "react";
 import {Inter} from "next/font/google";
 
 const inter = Inter({subsets: ["latin"]});
 
-export default function Home() {
+export default function UserLayout({children}: PropsWithChildren) {
   return (
     <main className={`h-screen w-screen flex justify-center items-center ${inter.className}`}>
-      <p className="text-8xl font-bold">HBKU Class Scheduler</p>
-
+      {children}
     </main>
   )
 }
-
-Home.layout = UserLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {ComponentType, ReactNode} from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type Props = {
+  Component: AppProps["Component"] & { layout?: ComponentType }
+  pageProps: AppProps["pageProps"]
+}
+
+const defaultLayout = ({children}: { children: ReactNode }) => <>{children}</>;
+
+export default function App({Component, pageProps}: Props) {
+  const Layout = Component.layout || defaultLayout;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
