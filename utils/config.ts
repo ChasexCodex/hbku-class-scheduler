a9563d49417cc8f5baf93d4c438d22aa4d018643
elyasamri <elyas.amri@gmail.com>
2024-06-20 12:08:06 +0300

    fix env variables and connect to emulators

---
+++ utils/config.ts
@@ -0,0 +1,39 @@
+const required = (name?: string) => {
+  if (!name) {
+    throw new Error(`Missing required environment variable ${name}`)
+  }
+
+  return name
+}
+
+const optional = (name?: string, def?: string) => {
+  return name || def
+}
+
+const configData = {
+  env: optional(process.env.NEXT_PUBLIC_ENV, 'production'),
+  firebase: {
+    apiKey: required(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
+    authDomain: required(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
+    databaseUrl: required(process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL),
+    projectId: required(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+    storageBucket: required(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
+    messagingSenderId: required(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+  }
+}
+
+type ConfigKey = keyof typeof configData
+type ConfigData = typeof configData
+
+const config = <T extends ConfigKey>(key: T, def?: any): ConfigData[T] => {
+  const value = configData[key]
+  if (value === undefined) {
+    if (def === undefined) {
+      throw new Error(`Config key ${key} is not defined`)
+    }
+    return def
+  }
+  return value as ConfigData[T]
+}
+
+export default config
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,13 +1,16 @@
 import {initializeApp} from "firebase/app";
+import {getFirestore, connectFirestoreEmulator} from "firebase/firestore";
+import {getAuth, connectAuthEmulator} from "firebase/auth";
+import config from "@/utils/config";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-};
+const firebaseConfig = config('firebase')
 
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const firestore = getFirestore(app)
+
+
+if (config('env') === 'development') {
+  connectAuthEmulator(auth, 'http://localhost:9099')
+  connectFirestoreEmulator(firestore, 'localhost', 8080)
+}
