32722785edbf0120846a80bf0614a3d117233b14
elyasamri <elyas.amri@gmail.com>
2024-07-09 16:32:48 +0300

    add state helpers

---
+++ hooks/state.ts
@@ -0,0 +1,20 @@
+import {useState} from "react";
+
+export const useArrayState = <T>(initialValue: T[] | (() => T[])) => {
+  const [value, setValue] = useState(initialValue);
+  const add = (item: T) => setValue([...value, item]);
+  const remove = (index: number) => setValue(value.filter((_, i) => i !== index));
+  const update = (updatedItem: T, index: number) => setValue(value.map((item, i) => index === i ? updatedItem : item));
+
+  return {value, setValue, add, remove, update}
+}
+
+export const useObjectState = <T extends object>(initialState: T | (() => T)) => {
+  const [state, setState] = useState(initialState);
+
+  const updateField = <F extends keyof T>(field: F, value: T[F]) => {
+    setState(prevState => ({ ...prevState, [field]: value }));
+  };
+
+  return {state, updateField};
+};
